6452f150b1cfedfc9071f7dff803199e5ab8dd19
Kyle Allan <kallan@riotgames.com>
2012-07-27 14:25:43 -0700

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             "groovy"
+maintainer       "YOUR_NAME"
+maintainer_email "YOUR_EMAIL"
+license          "All rights reserved"
+description      "Installs/Configures groovy"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: groovy
+# Recipe:: default
+#
+# Copyright (C) 2012 Kyle Allan (<kallan@riotgames.com>)
+# 
+# All rights reserved - Do Not Redistribute
+#
