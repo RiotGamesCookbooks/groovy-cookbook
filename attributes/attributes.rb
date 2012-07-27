2123a4c53bcd193e95cd8b4d5e312615ec59c256
Kyle Allan <kallan@riotgames.com>
2012-07-27 14:56:25 -0700

    getting groovy isntalled

---
+++ attributes/attributes.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: groovy
+# Attributes:: default
+#
+# Copyright (C) 2012 Kyle Allan (<kallan@riotgames.com>)
+# 
+# All rights reserved - Do Not Redistribute
+#
+default[:groovy][:version]    = '2.0.1'
+default[:groovy][:home]       = '/usr/local/groovy'
+default[:groovy][:url]        = 'http://dist.groovy.codehaus.org/distributions/groovy-binary-2.0.1.zip'
+default[:groovy][:checksum]   = '9bb87ed77a48a9f2ff05dbb70413660bab120eced68d4375fbac7273ebbe300a'
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,22 @@
 # 
 # All rights reserved - Do Not Redistribute
 #
+
+include_recipe "java"
+include_recipe "ark"
+
+ark "groovy" do
+  url node[:groovy][:url]
+  checksum node[:groovy][:checksum]
+  home_dir node[:groovy][:home]
+  version node[:groovy][:version]
+  append_env_path true
+  action :install
+end
+
+file "/etc/profile.d/groovy_home.sh" do
+  content <<-EOS
+    export GROOVY_HOME=#{node[:groovy][:home]}
+  EOS
+  mode 0755
+end
